01ddf656c44bd4d16d5b202b92a0f155653e4878
Peter Park <peterjpark@pm.me>
2023-03-30 01:10:51 -0400

    Add logic for checking if choice is correct

--- assets/script.js
+++ assets/script.js
@@ -51,10 +51,11 @@ var questionsList = [
 
 var landing = document.querySelector("#landing");
 var startBtn = document.querySelector("#start-btn");
+var scoreboardBtn = document.querySelector("#scoreboard-btn");
 var questionH3 = document.querySelector("#question");
-var questionNumberH3 = document.querySelector("#question-number")
+var questionNumberH3 = document.querySelector("#question-number");
 var questionTypeImg = document.querySelector("#question-type");
-
+var answersContainer = document.querySelector("#answers-container");
 
 // Utility to shuffle order of arrays. Use to shuffle questionsList array and answers array.
 function shuffle(array) {
@@ -66,13 +67,17 @@ function shuffle(array) {
     [array[i], array[randomI]] = [array[randomI], array[i]];
 
     // When shuffling answers, keep track of the correct answer's index.
-    i == correctI ? (correctI = randomI) : (correctI = i);
+    if (i == correctI) correctI = randomI;
+    else if (randomI == correctI) correctI = i;
   }
-  return array[correctI];
+  return correctI;
 }
 
 function startQuiz() {
   var currentQuestion = 0;
+  var correctAnswer = 0;
+  var points = 0;
+
   // Remove homepage.
   landing.style.display = "none";
 
@@ -81,18 +86,48 @@ function startQuiz() {
   function populateQuiz() {
     var question = questionsList[currentQuestion].question;
     var questionType = questionsList[currentQuestion].category;
+    var answers = questionsList[currentQuestion].answers;
 
-    console.log(questionsList);
-    console.log(questionType);
     // Populate question.
-    questionH3.innerHTML = "Question Text One";
+    questionH3.innerHTML = question;
     questionNumberH3.innerHTML = "Question " + (currentQuestion + 1);
     questionTypeImg.setAttribute("src", `./assets/images/${questionType}.svg`);
 
+    // Shuffle answers and keep track of correct answer's index.
+    correctAnswer = shuffle(answers);
 
+    // Populate answers.
+    for (i in answers) {
+      var answerBtn = document.createElement("button");
+      answersContainer.appendChild(answerBtn);
+      answerBtn.innerHTML = answers[i];
+      answerBtn.addEventListener("click", checkAnswer);
+      //Add marker to correct answer.
+      if (i == correctAnswer) {
+        answerBtn.setAttribute("data-correct", "");
+      }
+    }
+  }
+
+  function checkAnswer(e) {
+    if (
+      e.target.hasAttribute("data-correct") ||
+      e.target.parentElement.hasAttribute("data-correct")
+    ) {
+      points += 10;
+      alert("correct");
+    } else {
+
+      alert("incorrect");
+    }
   }
 
   populateQuiz();
 }
 
+function showScoreboard() {
+  console.log("show scoreboard");
+}
+
 startBtn.addEventListener("click", startQuiz);
+scoreboardBtn.addEventListener("click", showScoreboard);
